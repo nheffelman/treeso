5e47a219e088c7f7a855c1d2fc6e90bfcbaf3328
Nathan Heffelman <nheffelman@gmail.com>
2023-06-01 20:00:30 -0500

    data structure improved

--- Treeso.py
+++ Treeso.py
@@ -1,3 +1,4 @@
+#pylint:disable=E0001
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDFloatingActionButtonSpeedDial
@@ -8,7 +9,7 @@ import pickle
 from kivy.lang import Builder
 from kivymd.uix.card import MDCard
 from kivy.factory import Factory
-from kivy.properties import ListProperty, StringProperty, BooleanProperty
+from kivy.properties import ListProperty, StringProperty, BooleanProperty, NumericProperty, DictProperty
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.color_definitions import colors
 from kivymd.uix.tab import MDTabsBase
@@ -38,13 +39,13 @@ def unpickle_settings():
         settings = {}  
     return settings
 
-# pickles tree
+# pickles trees
 def pickle_tree(trees):
     with open('pickles/trees.pickle', 'wb') as handle:
         pickle.dump(trees, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-# unpickles treed
+# unpickles trees
 def unpickle_trees():
     if os.path.exists('pickles/trees.pickle'):
         with open('pickles/trees.pickle', 'rb') as handle:
@@ -109,16 +110,17 @@ class AccountScreen(MDScreen):
 
 class TreeScreen(MDScreen):
     text = StringProperty()
-
+    index = NumericProperty()
+    tree = DictProperty()
     def on_enter(self):
         self.add_widget(MDFloatingActionButtonSpeedDial(
             data={
                 'text': [
                     'text',
-                    "on_release", lambda x: self.add_text()
+                    "on_release", lambda x: self.add_text(self.tree)
                 ],
                 'Link': ['link', 
-                          "on_press",lambda x: self.add_link_url()],
+                          "on_press",lambda x: self.add_link_url(self.tree, add_link=True)],
                 'Video': [
                     'video', 
                     "on_press", lambda x: print('video pressed')
@@ -131,25 +133,32 @@ class TreeScreen(MDScreen):
             root_button_anim=True,
         ))
 
-    def load_tree(self, index, trees):
-        self.index = index
-        self.trees = trees
-        print('index: ', index)
-        trees = unpickle_trees()
-        tree=trees['tree_list'][index]
-        print(tree)
-        for leaf in tree['text']:
-            card = self.manager.get_screen('home').get_card(0)
-            card.text = leaf
-            card.bind(on_press = lambda widget: self.edit_text(text=leaf))
-            #namelabel = NameLabel(text=leaf, valign='top', halign='center', 
-                                  #on_press=lambda x: self.edit_text(text=leaf))
-            self.ids.leaves.add_widget(card)
+    def load_tree(self, tree):
+        self.tree = tree
+        leaves =  MDBoxLayout(orientation='vertical',
+            id='leaves',
+            adaptive_height=True,
+            spacing="56dp")
+        leafIndex=0
+        for leaf in tree['leaves']:
             
+            card = self.manager.get_screen('home').get_card(0)
+            if leaf.get('kind') == 'text':
+                card.text = leaf['text']
+                card.bind(on_press = lambda widget , tree=tree, leafIndex=leafIndex, leaf=leaf: self.edit_text(tree=tree, text=leaf['text'], leafIndex=leafIndex, add_text=False))
+                leaves.add_widget(card)
+            if leaf.get('kind') == 'link':
+                card.text = '#'+leaf['link']
+                card.bind(on_press = lambda widget , tree=tree, leafIndex=leafIndex, leaf=leaf: self.edit_link(tree=tree, text=leaf['link'], leafIndex=leafIndex, add_link=False))
+                leaves.add_widget(card)
+            leafIndex += 1
+        self.ids.box.add_widget(leaves)
+        widget = Widget()
+        self.ids.box.add_widget(widget)
 
-    def edit_text(self, text="", add_text=False):
+    def edit_text(self, tree, text="", leafIndex=0, add_text=False):
         print('edit text')
-        self.ids.topbar.left_action_items = [['close', lambda x: self.cancel_edit()]]
+        self.ids.topbar.left_action_items = [['close', lambda x: self.cancel_edit(tree)]]
         
         self.ids.box.remove_widget(self.ids.box.children[0])
         self.ids.box.remove_widget(self.ids.box.children[0])
@@ -157,61 +166,75 @@ class TreeScreen(MDScreen):
         self.ids.box.add_widget(textinput)
         widget = Widget()
         self.ids.box.add_widget(widget)
-        self.ids.topbar.right_action_items = [['check', lambda x, textinput=textinput : self.save_text(textinput, add_text)]]
+        self.ids.topbar.right_action_items = [['check', lambda x, textinput=textinput, tree=tree : self.save_text(tree, textinput, leafIndex, add_text)]]
 
-    def cancel_edit(self):
+    def cancel_edit(self, tree):
         self.ids.topbar.left_action_items = [['arrow-left', lambda x: self.home()]]
         self.ids.topbar.right_action_items = [['delete', lambda x: self.del_tree()],
                                               ['dots-vertical']]
         namelabel = NameLabel(text=self.text, valign='top', halign='center', 
                               on_press=lambda x: self.edit_text(self.text))
-        self.ids.box.remove_widget(self.ids.leaves.children[0])
-        self.ids.box.remove_widget(self.ids.leaves.children[0])
-        self.ids.box.add_widget(namelabel)
-        widget = Widget()
-        self.ids.box.add_widget(widget)
+        self.ids.box.remove_widget(self.ids.box.children[0])
+        self.ids.box.remove_widget(self.ids.box.children[0])
+        self.load_tree(tree)
     def home(self):
-        self.ids.leaves.clear_widgets()
+        self.ids.box.remove_widget(self.ids.box.children[0])
+        self.ids.box.remove_widget(self.ids.box.children[0])
         self.manager.current = 'home'
 
-    def save_text(self, textinput, add_text):
+    def save_text(self, tree, textinput, leafIndex, add_text):
         trees = unpickle_trees()
-        if 'tree_list' in trees:
-            tree_list = trees['tree_list']
-            if add_text:
-                tree_list[self.index]['text'].append(textinput.text)
-            else:
-                tree_list[self.index]['text'][0] = textinput.text
-            trees['tree_list'] = tree_list
-            pickle_tree(trees)
+        if add_text:
+
+            tree['leaves'].append({'kind': 'text', 'text': textinput.text})
+            
+        else:
+            tree['leaves'][leafIndex] = {'kind': 'text', 'text': textinput.text}  
+             
+        tree_list = trees.get('tree_list')
+        id = tree.get('id')
+        print(' tree is a ',type(tree))
+        tree_list[id] = dict(tree)
+        trees['tree_list'] = tree_list
+        pickle_tree(trees)
+
         self.ids.topbar.left_action_items = [['arrow-left', lambda x: self.home()]]
         self.ids.topbar.right_action_items = [['delete', lambda x: self.del_tree()],
                                               ['dots-vertical']]
-        namelabel = NameLabel(text=textinput.text, valign='top', halign='center', 
-                              on_press=lambda x: self.edit_text(self.text))
-        self.ids.box.remove_widget(self.ids.box.children[0])
-        self.ids.box.remove_widget(self.ids.box.children[0])
-        self.ids.box.add_widget(namelabel)
-        widget = Widget()
-        self.ids.box.add_widget(widget)
-
-    #function adds more text to the tree
-    def add_text(self):
-        self.edit_text(add_text=True)
-
-    def add_link_url(self):
-        print('edit text')
-        self.ids.topbar.left_action_items = [['close', lambda x: self.cancel_edit()]]
         
         self.ids.box.remove_widget(self.ids.box.children[0])
         self.ids.box.remove_widget(self.ids.box.children[0])
-        textinput = MDTextField(id='textinput', halign='center', text=self.text)
+        self.load_tree(tree)
+
+    #function adds more text to the tree
+    def add_text(self, tree):
+        self.edit_text(tree, add_text=True)
+
+    def add_link_url(self, tree, text="", leafIndex=0, add_link=False):
+        print('edit link')
+        self.ids.topbar.left_action_items = [['close', lambda x: self.cancel_edit(tree)]]
+        
+        self.ids.box.remove_widget(self.ids.box.children[0])
+        self.ids.box.remove_widget(self.ids.box.children[0])
+        textinput = MDTextField(id='textinput',multiline=True, halign='center', text=text)
         self.ids.box.add_widget(textinput)
         widget = Widget()
         self.ids.box.add_widget(widget)
-        self.ids.topbar.right_action_items = [['check', lambda x, textinput=textinput : self.save_link(textinput)]]
+        self.ids.topbar.right_action_items = [['check', lambda x, textinput=textinput, tree=tree : self.save_text(tree, textinput, leafIndex, add_link)]]
 
-    def save_link(self, textinput):
+    def save_link(self, tree, textinput, leafIndex, add_link):
+        trees = unpickle_trees()
+        if add_link:
+
+            tree['leaves'].append({'kind': 'link', 'link': textinput.text})
+            
+        else:
+            tree['leaves'][leafIndex] = {'kind': 'link', 'link': textinput.text}  
+             
+        tree_list = trees.get('tree_list')
+        id = tree.get('id')
+        print(' tree is a ',type(tree))
+        tree_list[id] = dict(tree)(self, textinput)
         trees = unpickle_trees()
         if 'tree_list' in trees:
             tree_list = trees['tree_list']
@@ -226,6 +249,16 @@ class TreeScreen(MDScreen):
             trees['tree_list'] = tree_list
             pickle_tree(trees)
             self.reload()
+        trees['tree_list'] = tree_list
+        pickle_tree(trees)
+
+        self.ids.topbar.left_action_items = [['arrow-left', lambda x: self.home()]]
+        self.ids.topbar.right_action_items = [['delete', lambda x: self.del_tree()],
+                                              ['dots-vertical']]
+        
+        self.ids.box.remove_widget(self.ids.box.children[0])
+        self.ids.box.remove_widget(self.ids.box.children[0])
+        self.load_tree(tree)
 
     def reload(self):
         self.ids.topbar.left_action_items = [['arrow-left', lambda x: self.home()]]
@@ -246,6 +279,8 @@ class TreeScreen(MDScreen):
         tree_list.remove(tree_list[self.index])
         trees['tree_list'] = tree_list
         pickle_tree(trees)
+        self.ids.box.remove_widget(self.ids.box.children[0])
+        self.ids.box.remove_widget(self.ids.box.children[0])
         self.manager.current = 'home'
 
     def add_tree_pressed(self):
@@ -286,7 +321,7 @@ class HomeScreen(MDScreen):
             },
             root_button_anim=True,
         ))
-
+        
         self.list_trees()
 
     def get_card(self, index):
@@ -325,48 +360,48 @@ class HomeScreen(MDScreen):
 
         self.ids.box.clear_widgets()
         trees = unpickle_trees()
-        
-        index = 0
+ 
         print(trees)
         if 'tree_list' in trees:
             tree_list = trees['tree_list']
-            trees_index = 0
+            index = 0 
+            lab = MDLabel(text=str(tree_list))
+            self.ids.box.add_widget(lab)
             for tree in tree_list:
                 card = self.get_card(index)
                 index += 1
                 if index > 2:
                     index = 0   
-                card.text = tree['text'][0]
-                card.bind(on_press = lambda widget, trees_index=trees_index, trees=trees: self.edit_tree(trees_index, trees))
-                trees_index += 1
+                card.text = tree['leaves'][0]['text']
+                card.bind(on_press = lambda widget, tree=tree: self.edit_tree(tree))
                 print(card)
                 self.ids.box.add_widget(card)
        
 
     def add_tree_pressed(self):
-        toast('tree pressed')
         trees = unpickle_trees()
         print(trees)
         if 'tree_list' in trees:
             tree_list = trees['tree_list']
         else:
+            id=0
             tree_list = []
         new_tree = {}
-        textlist = []
-        textlist.append('new tree')
-        new_tree['text'] = textlist
-        new_tree['index '] = len(tree_list)
+        leaves = []
+        leaf = {'kind': 'text', 'text': 'new tree'}
+        leaves.append(leaf)
+        new_tree['leaves'] = leaves
+        new_tree['id'] = len(tree_list)
         tree_list.append(new_tree)
         trees['tree_list'] = tree_list
         print(trees)
         pickle_tree(trees)
         self.list_trees()
 
-    def edit_tree(self, index, trees):
-        print(index, 'tree index')
-        print(trees)
+    def edit_tree(self, tree):
+        print(tree)
         self.manager.current = 'tree'
-        self.manager.get_screen('tree').load_tree(index, trees)
+        self.manager.get_screen('tree').load_tree(tree)
 
 class Picker(MDScreen):
     title = "Colors definitions"
